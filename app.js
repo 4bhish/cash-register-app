ce208e9c50355bad61bf02509fd333bd8f14e601
4bhish <108976136+4bhish@users.noreply.github.com>
2022-09-30 14:06:50 +0530

    chore input build in JS.
    updated input processing in JS.

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven =  document.querySelector("#cash-given");
 const btnCheck = document.querySelector("#check-button");
+const errorMSg = document.querySelector("#error-msg");
 
 btnCheck.addEventListener("click", function validateAmount()
 {
-    console.log(billAmount.value-cashGiven.value);
+    if(billAmount.value > 0)
+    {
+        if(cashGiven.value >= billAmount.value)
+        {
+            const amountReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountReturned);
+
+        }
+        else
+        {
+            errorMSg.innerText = "Entered Cash Given Amount must be greater or equal than Bill Amount"
+        }
+
+
+    }
+    else
+    {
+      errorMSg.innerText = " Entered Bill Amount must be greater than 0."
+    }
 })
